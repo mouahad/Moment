2125bd034a56c7f69406746fadc6ca00ceaeedd2
MYD <myd@macbook-air-de-myd.home>
2018-06-29 00:12:18 +0200

    ajout menu collection

---
+++ Moment/Vue/MomentCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MomentCollectionViewCell.swift
+//  Moment
+//
+//  Created by MYD on 28/06/2018.
+//  Copyright © 2018 MYD. All rights reserved.
+//
+
+import UIKit
+
+class MomentCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
